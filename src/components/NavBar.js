905231ab528483f7493d1aa10ea4a4f9c1094d67
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-11-30 20:24:25 +0000

    React router dom installed and routing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import { Navbar, Container, Nav} from 'react-bootstrap'
-import logo from "../assets/productivity_logo.jpg"
-import styles from "../styles/NavBar.module.css"
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo from "../assets/productivity_logo.jpg";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from 'react-router-dom';
 
 
 const NavBar = () => {
-  return ( 
-    <Container>
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
-            <Navbar.Brand> <img src={logo} alt="logo" height="45" /></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto text-left">
-                    <Nav.Link href="#home"> <i className='fas fa-home'></i>Home</Nav.Link>
-                    <Nav.Link href="#link"> <i className='fas fa-sign-in-alt'></i>Sign in</Nav.Link>
-                    <Nav.Link href="#link"> <i className='fas fa-user-plus'></i>Sign up</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </Container>
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <NavLink to="/"> <i className='fas fa-home'></i> Home</NavLink>
+            <NavLink to="/signin"> <i className='fas fa-sign-in-alt'></i> Sign in</NavLink>
+            <Nav.Link to="/signup"> <i className='fas fa-user-plus'></i> Sign up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>  
   );
 };
 
-export default NavBar
+export default NavBar;
