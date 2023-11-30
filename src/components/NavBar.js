63d6dda0b8ac70ab69715a9cf8639576d9e7c40e
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-11-30 11:33:42 +0000

    Created basic navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Navbar, Container, Nav} from 'react-bootstrap'
+import logo from "../assets/productivity_logo.jpg"
+
+
+const NavBar = () => {
+  return ( 
+    <Container>
+        <Navbar expand="md" fixed="top">
+            <Navbar.Brand> <img src={logo} alt="logo" height="45" /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto text-left">
+                    <Nav.Link href="#home"> <i className='fas fa-home'></i>Home</Nav.Link>
+                    <Nav.Link href="#link"> <i className='fas fa-sign-in-alt'></i>Sign in</Nav.Link>
+                    <Nav.Link href="#link"> <i className='fas fa-user-plus'></i>Sign up</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </Container>
+  );
+};
+
+export default NavBar
