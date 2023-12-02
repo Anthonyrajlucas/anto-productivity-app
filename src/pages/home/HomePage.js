b2bd066386fdcdefdcb9a8e6cd05cc131d78454f
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-12-02 22:36:08 +0000

    Add Home page text

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -3,15 +3,16 @@ import {Card} from 'react-bootstrap'
 
 
 
+
 const HomePage = () => {
   return (
     <>
   <Card>
-    <Card.Img variant="top" src="../" />
+  <img src='../assets/landing_page.jpg' alt='landingimage' />
     <Card.Body>
       <Card.Text>
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
+      <h1>Welcome to Anto-Productivity-App
+Unlock Your Productivity Potential with [Anto-Productivity-App], Your Ultimate Companion for Achieving More Every Day.</h1>
       </Card.Text>
     </Card.Body>
   </Card>
@@ -19,8 +20,38 @@ const HomePage = () => {
   <Card>
     <Card.Body>
       <Card.Text>
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
+      Why Choose Anto-Productivity-App?
+1. Seamless Organization:
+Streamline your life with our intuitive organization tools. From tasks and projects to notes and schedules, [Your App Name] keeps everything in one place for maximum efficiency.
+
+2. Smart Task Management:
+Prioritize tasks effortlessly using our intelligent task management system. Stay focused on what matters most, and let [Your App Name] handle the rest.
+
+3. Collaborate with Ease:
+Boost teamwork with collaborative features. Share projects, delegate tasks, and communicate seamlessly within the app, ensuring everyone stays on the same page.
+
+4. Time Tracking and Analytics:
+Understand how you spend your time with our powerful analytics. Identify patterns, set goals, and optimize your workflow for peak productivity.
+
+How Anto-Productivity-App Works
+Sign Up in Seconds:
+Get started with Anto-Productivity-App in a matter of seconds. No complicated setup just sign up and dive into a world of productivity.
+
+Intuitive Interface:
+Our user-friendly interface makes navigating [Your App Name] a breeze. Spend less time figuring out the app and more time getting things done.
+
+Customizable to Fit Your Needs:
+Tailor Anto-Productivity-App to match your unique workflow. Customizable features ensure the app adapts to you, not the other way around.
+
+What Our Users Say
+"Since using Anto-Productivity-App, my productivity has soared. I finally feel in control of my tasks and projects." - Amanda, Freelancer
+
+"The collaborative features in Anto-Productivity-App You have transformed the way my team works. It's a game-changer!" - John, Project Manager
+
+Start Your Productivity Journey Today
+Ready to take control of your time and accomplish more? Sign up for Anto-Productivity-App today and experience the future of productivity.
+
+Get Started for Free
       </Card.Text>
     </Card.Body>
     <Card.Img variant="bottom" src="holder.js/100px180" />
