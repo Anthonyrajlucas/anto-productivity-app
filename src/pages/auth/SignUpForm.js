b16ed4cafd42df0ef1c314ff20078de916da7571
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-12-02 19:53:09 +0000

    Add react bootstarap form in signupform.js

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
-
 import appStyles from "../../App.module.css";
-import {  Image, Col, Row, Container } from "react-bootstrap";
+import {  Form, Button, Image, Col, Row, Container } from "react-bootstrap";
 
 const SignUpForm = () => {
   return (
@@ -11,8 +10,17 @@ const SignUpForm = () => {
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
-
-            {/* add your form here */}
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">Submit</Button>
+          </Form> 
 
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
