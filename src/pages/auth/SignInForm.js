645410e32910310f69daa1f95319180de912cf1a
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-12-03 22:28:04 +0000

    Returns JSX sign-in form using Bootstrap's grid system (Row, Col, and Container)

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,38 +1,53 @@
-import React from 'react'
-import {Form, Button} from 'react-bootstrap'
+import React from "react";
+import { Form, Button, Col, Row, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import styles from "../../styles/CredentialsForm.module.css";
 import appStyles from "../../App.module.css";
-
-
-
 
 const SignInForm = () => {
   return (
-    <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+    <Row className={styles.Row}>
+      <Col className="col-sm-6 mx-auto" md={6}>
+        <Container className={`${styles.Form} p-4 `}>
+          <h1 className={styles.Header}>Sign In!</h1>
+          <p>Please enter your credentials below.</p>
+          
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+                className={styles.Input}
+        
+              />
+            </Form.Group>
+            
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-  <p>Dont have an account?</p>
-<Link to="/signup">
-  <p>Click <span className={appStyles.DarkText}>here </span>to Sign up!</p>
-</Link>
-</Form>
-  )
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                className={styles.Input}
+                
+              />
+            </Form.Group>
+            
+            <Button className={appStyles.Button} type="submit">
+              Log in
+            </Button>
+            
+          <p className={`${appStyles.DarkText} ${styles.TopMargin}`}>Dont have an account?</p>
+          <Link to="/signup">
+          <p className={`${appStyles.DarkText}`}>Click <span className={appStyles.DarkText}>here </span>to Sign up!</p>
+          </Link>
+        </Container>
+      </Col>
+    </Row>
+  );
 }
 
-export default SignInForm
+export default SignInForm;
+
