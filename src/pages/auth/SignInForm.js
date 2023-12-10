fd4c8a9642317b88bade07f36aad2e5387eee86b
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-12-10 16:28:39 +0000

    Sigin form code changed

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,17 +1,53 @@
-import React from "react";
-import { Form, Button, Col, Row, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import styles from "../../styles/CredentialsForm.module.css";
+import React, { useState } from "react";
+import axios from "axios";
+
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+
+import { Link, useHistory } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-const SignInForm = () => {
+function SignInForm() {
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <Row className={styles.Row}>
-      <Col className="col-sm-6 mx-auto" md={6}>
-        <Container className={`${styles.Form} p-4 `}>
-          <h1 className={styles.Header}>Sign In!</h1>
-          <p>Please enter your credentials below.</p>
-          
+      <Col className="my-auto p-0 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>sign in</h1>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
@@ -19,10 +55,15 @@ const SignInForm = () => {
                 placeholder="Username"
                 name="username"
                 className={styles.Input}
-        
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
-            
+            {errors.username?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="password">
               <Form.Label className="d-none">Password</Form.Label>
@@ -31,23 +72,45 @@ const SignInForm = () => {
                 placeholder="Password"
                 name="password"
                 className={styles.Input}
-                
+                value={password}
+                onChange={handleChange}
               />
             </Form.Group>
-            
-            <Button className={appStyles.Button} type="submit">
-              Log in
+            {errors.password?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
+            >
+              Sign in
             </Button>
-            
-          <p className={`${appStyles.DarkText} ${styles.TopMargin}`}>Dont have an account?</p>
-          <Link to="/signup">
-          <p className={`${appStyles.DarkText}`}>Click <span className={appStyles.DarkText}>here </span>to Sign up!</p>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account? <span>Sign up now!</span>
           </Link>
         </Container>
+      </Col>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage}`}
+          src={"https://res.cloudinary.com/df8bd97ok/image/upload/v1702225418/pellerex-idntity-signin_urz6sr.png"}
+        />
       </Col>
     </Row>
   );
 }
 
 export default SignInForm;
-
