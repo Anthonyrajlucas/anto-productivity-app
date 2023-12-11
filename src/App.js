9cc9e8b1a0d21fb1e964309ce79ccf7c5f9447ce
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-12-11 18:15:15 +0000

    export two newly created axios instances

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import "./api/axiosDefaults";
 
+
 function App() {
   return (
     <div className={styles.App}>
     <NavBar />
     <Container className={styles.Main}>
       <Switch>
-        <Route exact path="/" render={() => <h1>Home page</h1>} />
+        <Route exact path="/" render={() => <h1>Home Page</h1> } />
         <Route exact path="/signin" render={() => <SignInForm />} />
         <Route exact path="/signup" render={() => <SignUpForm />} />
         <Route render={() => <p>Page not found!</p>} />
--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://anto-productivity-app-backend-6c122e357cb1.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
